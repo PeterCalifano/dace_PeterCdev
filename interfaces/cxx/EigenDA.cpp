754518a56c3ead5a67392977eb154f5da7c4b57e
PeterC <petercalifano.gs@gmail.com>
2024-07-04 10:55:10 +0200

    Tentative prototype of wrapper for Eigen vector and DA vectors

---
+++ interfaces/cxx/EigenDA.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file EigenDA.cpp
+ * @author PeterC (petercalifano.gs@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-07-04
+ */
+
+// DACE classes
+#include "dace/config.h"
+#include "dace/DA.h"
+#include "dace/AlgebraicMatrix.h"
+#include "dace/AlgebraicMatrix_t.h"
+
+
+namespace EigenDA {
+
+}
---
+++ interfaces/cxx/include/EigenDA.hpp
@@ -0,0 +1,42 @@
+// C++ stdlib classes required for interface definition
+#include <vector>
+#include <iostream>
+
+// DACE classes required for interface definition
+#include "dace/PromotionTrait.h"
+#include <Eigen/Dense>
+
+
+namespace EigenDA {
+
+    // Define the EigenDA class here
+    template<typename T> VectorXd(const T &obj) {
+        public:
+            // CONSTRUCTOR
+            VectorXd(const T &obj)
+            {
+                // TODO
+        }
+
+
+        public:
+            // PUBLIC DATA MEMBERS
+
+        public:
+            // PUBLIC METHODS
+
+        public: 
+        
+        // OPERATOR OVERLOADS
+        VectorXd operator+(const VectorXd &other) const;
+        VectorXd operator*(const VectorXd &other) const;
+        double operator()(int row, int col) const;
+        double operator<<(std::ostream &os, const VectorXd &obj);
+
+        private:
+            // PRIVATE DATA MEMBERS
+            Eigen::VectorXd eigenVector;
+            DA::AlgebraicVector daceVector;
+    }
+
+}
